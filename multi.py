866bc5d922b21e6b75637a9046d040595cc64ca4
annaoskarson <58904189+annaoskarson@users.noreply.github.com>
2021-02-25 21:05:47 +0100

    Update multi.py

--- multi.py
+++ multi.py
@@ -3,34 +3,6 @@ import termcolor
 import os
 
 def main():
-    clear = lambda: os.system('clear')
-
-    width = os.get_terminal_size()[0]
-    height = os.get_terminal_size()[1]
-
-    if len(sys.argv) == 1:
-        tables = list(range(11))
-    elif '--' in sys.argv[1]:
-        print("\nFör att välja tabeller, skriv '1-4' eller '2,6,5' som argument.")
-        print("Skriver du inget, testas tabellerna 0-10.\n")
-        return()
-    elif ',' in sys.argv[1]:
-        tables = list(map(int, sys.argv[1].split(',')))
-    elif '-' in sys.argv[1]:
-        fr,to = map(int, sys.argv[1].split('-'))
-        tables = list(range(fr,to+1))
-    elif sys.argv[1].isnumeric():
-        tables = [int(sys.argv[1])]
-
-    questions = []
-    for i in tables:
-        for j in range(11):
-            questions.append((i,j))
-
-    correct = []
-    car = random.choice(['🚃','🚌','🚕','🚐','🚑','🚒','🚓','🚴','🚶','🛻',
-                    '🛸','🚗','🚚','🚙','🚜','🚂','🛥','🛺','🛰','🚣','🚤',
-                    '🛷','⛷','🛶'])
 
     def fireworks(string):
         col = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
@@ -84,37 +56,73 @@ def main():
             + goal + path + car + after + start
             + '\n\n')
 
+    clear = lambda: os.system('clear')
+    width = os.get_terminal_size()[0]
+    height = os.get_terminal_size()[1]
+
+    if len(sys.argv) == 1:
+        tables = list(range(11))
+    elif '--' in sys.argv[1]:
+        print("\nFör att välja tabeller, skriv '1-4' eller '2,6,5' som argument.")
+        print("Skriver du inget, testas tabellerna 0-10.\n")
+        return()
+    elif ',' in sys.argv[1]:
+        tables = list(map(int, sys.argv[1].split(',')))
+    elif '-' in sys.argv[1]:
+        fr,to = map(int, sys.argv[1].split('-'))
+        tables = list(range(fr,to+1))
+    elif sys.argv[1].isnumeric():
+        tables = [int(sys.argv[1])]
+
+    questions = []
+    for i in tables:
+        for j in range(11):
+            questions.append((i,j))
+
+    correct = []
+    car = random.choice(['🚃','🚌','🚕','🚐','🚑','🚒','🚓','🚴','🚶','🛻',
+                    '🛸','🚗','🚚','🚙','🚜','🚂','🛥','🛺','🛰','🚣','🚤',
+                    '🛷','⛷','🛶'])
 
     question = random.choice(questions)
     total = len(questions)
+    practise = False
     while True:
         score = (len(correct) + (total-len(questions))) / (2*total)
         clear()
         print(scoring(score, car))
+        t0 = time.time()
         ans = input('     ' + str(question[0]) + ' ⋅ ' + str(question[1]) + ' = ')
         if not(ans.isnumeric()):
-            print('     Du måste skriva siffror. Försök igen.\n\n     Tryck på enter (⮐ ) för att fortsätta.')
-            input('     ')
+            print('\n     Du måste skriva siffror. Försök igen.\n\n     Tryck på enter (⮐ ) för att fortsätta.')
+            input('\n     ')
         elif int(ans) == question[0]*question[1]:
-            if question in correct:
+            t1 = time.time()
+            tdiff = t1-t0
+            if practise:
+                practise = False
+            elif question in correct:
                 questions.remove(question)
             else:
                 correct.append((question))
+                if tdiff < 6:
+                    questions.remove(question)
             if len(questions) > 0:
                 question = random.choice(questions)
             else:
                 clear()
                 score = 1
-                string = 'Du har svarat rätt på alla uppgifter två gånger. Grattis!'
+                string = 'Du kan det här bra nu. Grattis!'
                 padding = ' ' * int(round(width - len(string)) /2)
                 fireworks(scoring(score, car) + padding + string)
                 return()
         else:
             clear()
+            practise = True
             print(scoring(score, car))
-            print('     ' + str(question[0]) + ' * ' + str(question[1]) + ' ≠ ' + ans)
+            print('     ' + str(question[0]) + ' ⋅ ' + str(question[1]) + ' ≠ ' + ans)
             print('\n     Rätt svar är ' + str(question[0]*question[1]) + '.')
             print('\n     Försök igen! Tryck på enter (⮐ ) för att fortsätta.')
-            input('     ')
+            input('\n     ')
 
 main()
